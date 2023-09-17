4d6fbb4a8bcf1319b7accdfe8b044a48e795f52c
Nicolas <126731327+NicolasCorneli@users.noreply.github.com>
2023-09-17 20:51:27 -0300

    Create paste_menu.py

---
+++ paste_menu.py
@@ -0,0 +1,13 @@
+def menu():
+    print("1 - add produto")
+    print("2 - buscar produto")
+    print("3 - vizualisar todos os produtos")
+    print("4 - vender produto")
+    print("5 - relatorio de vendas")
+    print("6 - vizualisar por categoria")
+    print("7 - excluir um produto")
+    print("8 - alterar o valor de um produto")
+    print("9 - sair")
+    
+    opt = int(input("digite uma opt: "))
+    return opt
